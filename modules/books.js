0f1732686ac3c162354da1674e89a53c19194713
Wassim <wassimchakib97@gmail.com>
2022-10-31 10:44:11 +0100

    Fix functions to use ES6 syntax

--- modules/books.js
+++ modules/books.js
@@ -3,7 +3,7 @@ export default class Books {
     this.listOfBooks = JSON.parse(localStorage.getItem('books')) || [];
   }
 
-  addBook(title, author) {
+  addBook = (title, author) => {
     // Add new book into the existing array of books
     this.listOfBooks.push({
       title,
@@ -13,7 +13,7 @@ export default class Books {
     localStorage.setItem('books', JSON.stringify(this.listOfBooks));
   }
 
-  removeBook(id) {
+  removeBook = (id) => {
     // Remove the item that has index of id
     this.listOfBooks.splice(id, 1);
     // Replace old object of books with the new updated one
